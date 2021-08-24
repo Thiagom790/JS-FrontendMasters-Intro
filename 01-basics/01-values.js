59d92de766f1c1c342b8df1a28f0b22c7ac2f428
Thiago Marques <thiagom790@gmail.com>
2021-08-24 10:12:25 -0300

    first commit

---
+++ 01-basics/01-values.js
@@ -0,0 +1,16 @@
+//tipos de dados do js
+42;
+3.14;
+
+("Hello, friend");
+
+true;
+false;
+
+null;
+undefined;
+
+[1, 2, 4];
+{
+  name: "kyle";
+}
---
+++ 01-basics/02-operations.js
@@ -0,0 +1,12 @@
+3 + 4;
+43 - 1;
+
+"Kyle" + "simpson";
+
+!false;
+
+console.log(3.0 == 3);
+
+3 < 4;
+
+true || false;
